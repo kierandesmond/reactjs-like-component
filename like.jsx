7131b4dbfb16329fb496ee9137b2929bf67f3038
kierandesmond <34133202+kierandesmond@users.noreply.github.com>
2020-06-17 10:12:37 +0200

    Updated component to be stateless funcional comp
    Updated component to be stateless funcional component

--- like.jsx
+++ like.jsx
@@ -3,22 +3,17 @@ import FontAwesome  from "react-fontawesome";
 // Input : liked : boolean
 // Output : onClick event
 
-
-class Like extends Component
-{
-    state ={}
-    render() {
-        const iconName = this.props.liked ? "heart" : "heart-o";
+const Like = (props) =>{
+    const iconName = props.liked ? "heart" : "heart-o";
         return ( <i>
             <FontAwesome
             className="heart"
             name={iconName}
             style={{ cursor: 'pointer' }}
             size="2x"
-            onClick={ this.props.onClick}
+            onClick={ props.onClick}
             style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/>
            </i>
            );
-    }
 }
 export default Like;
